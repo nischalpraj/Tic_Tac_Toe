fe0a27a44459af46b82f9defdf4be3972d8ecc3b
nischalpraj <nischalpraj776@gmail.com>
2026-01-23 14:19:10 +0545

    Started js

---
+++ script.js
@@ -0,0 +1,8 @@
+//selecting all required element
+const selectbox = document.querySelector(".selectbox"),
+    selectXbtn = document.querySelector(".playerx"),
+    selectobtn = document.querySelector(".playero");
+
+window.onload = () => {//when window loads
+    
+    }
