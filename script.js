3a467fb742a1779a3dfacbe6feadb623067d1f29
nischalpraj <nischalpraj776@gmail.com>
2026-01-23 14:25:58 +0545

    first js finished

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //selecting all required element
 const selectbox = document.querySelector(".selectbox"),
-    selectXbtn = document.querySelector(".playerx"),
-    selectobtn = document.querySelector(".playero");
+  selectXbtn = document.querySelector(".playerx"),
+  selectobtn = document.querySelector(".playero");
 
-window.onload = () => {//when window loads
-    
-    }
+window.onload = () => {
+  //when window loads
+  selectXbtn.onclick = () => {
+    selectbox.classList.add("hide");
+  };
+};
