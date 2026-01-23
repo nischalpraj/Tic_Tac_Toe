3b5fadbe25ecf3b7eabefe8dca01a7bc67deecdb
nischalpraj <nischalpraj776@gmail.com>
2026-01-23 14:35:17 +0545

    Made the playboard appear

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 //selecting all required element
 const selectbox = document.querySelector(".selectbox"),
-  selectXbtn = document.querySelector(".playerx"),
-  selectobtn = document.querySelector(".playero");
+  selectXbtn = selectbox.querySelector(".playerx"),
+  selectobtn = selectbox.querySelector(".playero"),
+  playboard = document.querySelector(".playboard");
 
 window.onload = () => {
   //when window loads
   selectXbtn.onclick = () => {
+      selectbox.classList.add("hide");//hide the pplay box when x is clicked
+      playboard.classList.add("show");//show playboard when x is clicked
+  };
+  selectobtn.onclick = () => {
     selectbox.classList.add("hide");
   };
 };
