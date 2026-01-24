b2a981253caf26e8ef3738313d2909adc31f33e5
nischalpraj <nischalpraj776@gmail.com>
2026-01-24 13:15:46 +0545

    set onclick attribute

--- script.js
+++ script.js
@@ -2,11 +2,16 @@
 const selectbox = document.querySelector(".selectbox"),
   selectXbtn = selectbox.querySelector(".playerx"),
   selectobtn = selectbox.querySelector(".playero"),
-  allbox=document.querySelectorAll("section span"),
+  allbox = document.querySelectorAll("section span"),
   playboard = document.querySelector(".playboard");
 
 window.onload = () => {
   //when window loads
+
+  for (let i = 0; i < Array.length; i++) {
+    //add onclickt attritbute on all available section's span
+    (allbox[i], setAttribute("onclick", "clickedbox(this)"));
+  }
   selectXbtn.onclick = () => {
     selectbox.classList.add("hide"); //hide the pplay box when x is clicked
     playboard.classList.add("show"); //show playboard when x is clicked
